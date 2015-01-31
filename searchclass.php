b2a46f2dc27d6a9f001899181d1facc9a7e92c09
Zain Munad <zainm211@hotmail.com>
2015-01-30 17:51:24 -0800

    initial parse php test

---
+++ searchclass.php
@@ -0,0 +1,13 @@
+require 'vendor/autoload.php';
+ 
+use Parse\ParseClient;
+ 
+ParseClient::initialize('w65VqVKa8iGpK7hcHqJOkUkqaUuTvKi0yh3J6xGe', 'a7g8cy2WLjmbAuIj3KiaohHCSr1MvOYKA47tJvBM', 'NCURm6asi3YTOaukNJy3ySseoWiQvsOq44721V7D');
+
+use Parse\ParseObject;
+ 
+$testObject = ParseObject::create("TestObject");
+$testObject->set("foo", "bar");
+$testObject->save();
+
+echo "all done!";
