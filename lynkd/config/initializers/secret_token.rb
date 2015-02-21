16564683172076cb8518b9dff411a629e1651b22
root <root@clevrly.me>
2015-02-20 20:14:08 -0500

    Switch to rails

---
+++ lynkd/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lynkd::Application.config.secret_token = '9bb170a08b279c561cb678072494653c335e58214f0c4b3621827f9ce26c615c43ed2ce0e0e59e1dea3e9e57d31af1c1c3367875f3a0421035f1bf0af3467dc8'
